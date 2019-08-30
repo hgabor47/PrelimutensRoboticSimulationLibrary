5e34972770b600770c6499cdacbd299314ce03be
Gabor Horvath <hgabor47@gmail.com>
2019-08-30 14:03:33 +0200

    Update sunflower_demo.ino

--- examples/sunflower_demo.ino
+++ examples/sunflower_demo.ino
@@ -10,7 +10,7 @@ ESP8266WebServer BlenderServer(82);
 PreliSimRobotic BlenderService;
     
 #define STASSID0 "hgplsoft"
-#define STAPSK0  "7102237203210"
+#define STAPSK0  "xxx"
 #define LIMIT 5.0
     
 const char* otssid0 = STASSID0;
